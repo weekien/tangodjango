558573a8276fea70d077a850ca28ec1d634643dd
Chia Wee Kien <weekien@cyber-village.net>
2015-01-24 16:07:41 +0800

    Tango with django tutorial

---
+++ rango/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    return render(request, 'rango/index.html', context_dict)
+
+
+def about(request):
+    return HttpResponse(": Rango says here is the about page!")
